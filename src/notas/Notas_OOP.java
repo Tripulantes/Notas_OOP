bac063284a8251360ee84df11c4de74a65e7b9a3
yanyg <yanyg@DESKTOP-E0JBJ78>
2022-07-08 10:51:42 -0500

    Version terminada

--- src/notas/Notas_OOP.java
+++ src/notas/Notas_OOP.java
@@ -9,10 +9,16 @@ public class Notas_OOP {
  public static int numEst;
     
     public static void main(String[] args) {
+        double suma = 0;
+        
         Scanner sc = new Scanner(System.in);
         System.out.println("*****PROMEDIO Y ESTADISTICAS DE NOTAS ESTUDIANTE*****");
         System.out.println("Numero de estudiantes: ");
-        numEst=sc.nextInt();        
+        numEst=sc.nextInt();  
+        double [] resultados1 = new double[numEst];
+        double [] resultados2 = new double[numEst];
+        double [] resultados3 = new double[numEst];
+        Sistema_control_notas notas = new Sistema_control_notas(numEst);
         for( int tri=0; tri<numEst;tri ++){
             Tripulante t = new Tripulante();
             System.out.println("Ingrese numero de cedula: ");
@@ -31,12 +37,19 @@ public class Notas_OOP {
             t.setIngles(sc.nextDouble());
             System.out.println("Ingrese nota de habilidades presonales: ");
             t.setHabilidades_p(sc.nextDouble());
-            System.out.println(t.notaFinal());
+            //System.out.println(t.notaFinal());
             System.out.println(t.est_1());
+            resultados1[tri]=t.est_1();
+            resultados2[tri]=t.est_2();
+            resultados3[tri]=t.est_3();
             System.out.println(t.est_2());
             System.out.print(t.est_3());
-                
+            suma+=t.notaFinal();
         }
+        notas.calcularPromedio(suma);
+        notas.calcularMayorEstadistica(resultados1);
+        notas.calcularMayorEstadistica(resultados2);
+        notas.calcularMayorEstadistica(resultados3);
     }
     
 }
--- src/notas/Sistema_control_notas.java
+++ src/notas/Sistema_control_notas.java
@@ -5,10 +5,34 @@
  */
 package notas;
 
+import javax.print.attribute.standard.Sides;
+
 /**
  *
  * @author Drew
  */
 public class Sistema_control_notas {
-    
+    private int numEst;
+    //metodos calcular promedio, calcular estadistica 1 2 3 get
+    public Sistema_control_notas(int numEst){
+        this.numEst=numEst;
+    }
+    public double calcularPromedio(double acumulado){
+        double promedioTotal=acumulado/numEst;
+        return promedioTotal;
+    }
+    public int calcularMayorEstadistica(double[] estadisticas){
+        int indiceDelMayor = 0;
+		// Recorrer arreglo y ver si no es así
+		// (comenzar desde el 1 porque el 0 ya lo tenemos contemplado arriba)
+		for (int x = 1; x < estadisticas.length; x++) {
+			if (estadisticas[x] > estadisticas[indiceDelMayor]) {
+				indiceDelMayor = x;
+			}
+		}
+		// Ahora podemos obtener el mayor usando la posición
+		double mayor = estadisticas[indiceDelMayor];
+		System.out.println("El mayor en estadistica_1 es: " + mayor );
+        return 1;
+    }
 }
